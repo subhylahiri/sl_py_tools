93be4af3f1ad78c038dd7d7a65bf2983ed974f89
Subhaneil Lahiri <subhylahiri@gmail.com>
2018-09-16 17:57:53 -0700

    use gufuncs if possible

--- numpy_tricks/linalg/_linalg.py
+++ numpy_tricks/linalg/_linalg.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-# =============================================================================
-# Created on Thu Jul  6 12:10:02 2017
-# @author: Subhy
-# module: _linalg
-# =============================================================================
 """
+Created on Thu Jul  6 12:10:02 2017
+
+@author: Subhy
+
 Linear algebra routines.
 
 Functions
@@ -25,8 +24,9 @@ matmul
     Matrix multiplication with better type flexibility.
 """
 
-from typing import Tuple
 import numpy as np
+from typing import Tuple
+from . import _gufuncs as gf
 
 # =============================================================================
 # Reshaping for linear algebra
@@ -128,165 +128,83 @@ def return_shape_mat(x: np.ndarray, y: np.ndarray) -> Tuple[int, ...]:
 # =============================================================================
 
 
-def matldiv(x: np.ndarray, y: np.ndarray,
-            out=(None,), rcond=1e-15) -> np.ndarray:
+def matldiv(x: np.ndarray, y: np.ndarray, rcond=None,
+            *args, **kwargs) -> np.ndarray:
     """Matrix division from left.
 
-    Computes :math:`z = x \\ y = x^{-1} y`, or :math:`x^+ y` for nonsquare `x`.
-    Pseudo-inverse version broadcasts (if broadcasting is needed, uses
-    `np.linalg.solve` and assumes full rank `x`, else, uses `np.linalg.lstsq`).
-    Full inverse version broadcasts (uses `np.linalg.solve`).
+    Computes :math:`z = x \\ y = x^{-1} y` for square `x`, or :math:`x^+ y`
+    for rectangular `x`.
+    Pseudo-inverse version uses `gufunc.lstsq`.
+    Full inverse version uses `gufunc.solve`.
+    Both versions broadcast using gufunc machinery.
 
     Parameters
     ----------
-    x : (..., M, N) array_like
+    x : {(..., M, N), (..., N, N)} array_like
         Divisor or Denominator.
-    y : {(..., M), (..., M, K)} array_like
+    y : {(..., N), (..., N, K)} array_like
         Dividend or Numerator.
-    out : {(..., N), (..., N, K)} np.ndarray
-        array to store the output in.
-    rcond : float = 1e-15
+    rcond : optional float = None
         passed to `numpy.linalg.lstsq`
+    out : {(..., M), (..., M, K), (..., N), (..., N, K)} np.ndarray
+        array to store the output in.
 
     Returns
     -------
-    z : {(..., N), (..., N, K)} np.ndarray
-        Quotient. It has the same type as `a`, unless type(b) is a subclass of
-        type(a), in which case `c` has the same type as `b`.
+    z : {(..., M), (..., M, K), (..., N), (..., N, K)} np.ndarray
+        Quotient. It has the same type as `x` or `y`.
 
     Raises
     ------
     LinAlgError
-        If `x` is not full rank (square, or broadcasting non-square) or
-        if computation doesn't converge (non-square, no broadcasting).
+        If `x` is not full rank (square) or if computation doesn't converge
+        (rectangular).
 
     See also
     --------
     `np.linalg.solve` : performs exact matrix division.
     `np.linalg.lstsq` : performs least-square matrix division.
     """
-    a = np.asarray(x)
-    b = np.asarray(y)
-    if a.dtype == np.object_ or b.dtype == np.object_:
-        return NotImplemented
-
-    if isinstance(out, np.ndarray):
-        out = (out,)
-    if out[0] is None:
-        out = (_preallocate_out(trnsp(a), b),)
-
-    if a.ndim == 0:
-        out[0][...] = b / a
-    elif a.ndim == 1:
-        out[0][...] = a @ b / (a @ a)
-    elif a.shape[-2] == a.shape[-1]:
-        out[0][...] = np.linalg.solve(a, b)
-    elif a.ndim == 2 and b.ndim <= 2:
-        out[0][...] = np.linalg.lstsq(a, b, rcond=rcond)[0]
-    elif a.shape[-2] < a.shape[-1]:
-        out[0][...] = trnsp(a) @ np.linalg.solve(a @ trnsp(a), b)
-    elif a.shape[-2] > a.shape[-1]:
-        out[0][...] = np.linalg.solve(trnsp(a) @ a, trnsp(a) @ b)
-    else:
-        return NotImplemented
-
-    return out[0]
+    try:
+        return gf.solve(x, y, *args, **kwargs)
+    except (np.linalg.LinAlgError, ValueError):
+        pass
+    return gf.lstsq(x, y, rcond, *args, **kwargs)[0]
 
 
-def matrdiv(x: np.ndarray, y: np.ndarray, out=(None,),
-            *args, **kwargs) -> np.ndarray:
+def matrdiv(x: np.ndarray, y: np.ndarray, *args, **kwargs) -> np.ndarray:
     """Matrix division from right.
 
-    Computes :math:`z = x / y = x y^{-1}`, or :math:`x y^+` for nonsquare `y`.
-    Pseudo inverse version broadcasts  (if broadcasting is needed, uses
-    `np.linalg.solve` and assumes full rank `y`, else, uses `np.linalg.lstsq`).
-    Full inverse version broadcasts (uses `np.linalg.solve`).
+    Computes :math:`z = x / y = x y^{-1}`, or :math:`x y^+` if y isn't square.
+    Pseudo-inverse version uses `gufunc.lstsq`.
+    Full inverse version uses `gufunc.solve`.
+    Both versions broadcast using gufunc machinery.
 
     Parameters
     ----------
     x : {(..., M), (..., K, M)} array_like
         Dividend or Numerator.
-    y : (..., N, M) array_like
+    y : {(..., M, N), (..., M, M)} array_like
         Divisor or Denominator.
-    out : {(..., N), (..., K, N)} np.ndarray
-        array to store the output in.
     rcond : float = 1e-15
         passed to `numpy.linalg.lstsq`
+    out : {(..., N), (..., K, N), (..., M), (..., K, M)} np.ndarray
+        array to store the output in.
 
     Returns
     -------
-    z : {(..., N), (..., K, N)} np.ndarray
-        Quotient. It has the same type as `a`, unless type(b) is a subclass of
-        type(a), in which case `c` has the same type as `b`.
+    z : {(..., N), (..., K, N), (..., M), (..., K, M)} np.ndarray
+        Quotient. It has the same type as `x` or `y`.
 
     Raises
     ------
     LinAlgError
-        If b is not full rank (square, or broadcasting non-square) or
-        if computation doesn't converge (non-square, no broadcasting).
+        If `y` is not full rank (square) or if computation doesn't converge
+        (rectangular).
 
     See also
     --------
     `np.linalg.solve` : performs exact matrix division.
     `np.linalg.lstsq` : performs least-square matrix division.
     """
-    a = np.asarray(x)
-    b = np.asarray(y)
-    if a.dtype == np.object_ or b.dtype == np.object_:
-        return NotImplemented
-
-    if isinstance(out, np.ndarray):
-        out = (out,)
-    if out[0] is None:
-        out = (_preallocate_out(a, trnsp(b)),)
-
-    out[0][...] = trnsp(matldiv(trnsp(b), trnsp(a), *args, **kwargs))
-
-    return out[0]
-
-
-def matmul(x, y, out=(None,)):
-    """Matrix multiplication.
-
-    `np.matmul` doesn't pass through subclasses. This just fixes that.
-    Makes sure it returns NotImplemented instead of TypeError
-
-    See also
-    --------
-    `np.matmul`
-    """
-    a = np.asarray(x)
-    b = np.asarray(y)
-    if a.dtype == np.object_ or b.dtype == np.object_:
-        return NotImplemented
-
-    if isinstance(out, np.ndarray):
-        out = (out,)
-    if out[0] is None:
-        out = (_preallocate_out(a, b),)
-
-    np.matmul(a, b, out=out[0])
-
-    return out[0]
-
-
-def _mostsub_type(a, *args) -> type:
-    """Most derived type of arguments.
-
-    For a non-linear type hierachy, it takes the first branch, left to right.
-    """
-    returntype = type(a)
-    for b in args:
-        if isinstance(b, returntype):
-            returntype = type(b)
-    return returntype
-
-
-def _preallocate_out(a: np.ndarray, b: np.ndarray) -> np.ndarray:
-    """Preallocated array for output of a @ b
-    Correct shape, dtype and type
-    """
-    returntype = _mostsub_type(a, b)
-    returndtype = np.result_type(a, b)
-
-    return np.empty(return_shape_mat(a, b), dtype=returndtype).view(returntype)
+    return trnsp(matldiv(trnsp(y), trnsp(x), *args, **kwargs))
