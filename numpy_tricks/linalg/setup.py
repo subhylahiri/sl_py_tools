826aa3ab3e45a5cda16dcbcdf5a8ea2d15de2c8e
Subhaneil Lahiri <subhylahiri@gmail.com>
2018-12-06 01:56:30 -0800

    moved c code to src folder

--- numpy_tricks/linalg/setup.py
+++ numpy_tricks/linalg/setup.py
@@ -4,12 +4,18 @@ Created on Fri May  4 18:22:44 2018
 
 @author: Subhy
 """
-
+import os.path as osp
 from numpy.distutils.core import setup
 from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
 from numpy.distutils.misc_util import get_info as get_misc_info
 from distutils.sysconfig import get_python_inc
 from numpy.distutils.system_info import get_info as get_sys_info
+# =========================================================================
+
+
+def in_src(name):
+    return osp.join('src', name)
+
 
 # =========================================================================
 config = Configuration()
@@ -18,34 +24,35 @@ inc_dirs = [get_python_inc()]
 if inc_dirs[0] != get_python_inc(plat_specific=1):
     inc_dirs.append(get_python_inc(plat_specific=1))
 inc_dirs.append(get_numpy_include_dirs())
+inc_dirs.append('src')
 
 lapack_info = get_sys_info('lapack_opt', 0)  # and {}
 npymath_info = get_misc_info("npymath")
 all_info = {k: lapack_info[k] + npymath_info[k] for k in lapack_info.keys()}
-rearrange = 'rearrange_data.c.src'
+rearrange = in_src('rearrange_data.c.src')
 
 # =============================================================================
 config.add_extension('_gufuncs_cloop',
-                     sources=['gufuncs_cloop.c.src'],
+                     sources=[in_src('gufuncs_cloop.c.src')],
                      include_dirs=inc_dirs,
                      extra_info=npymath_info)
 # =============================================================================
 config.add_extension('_gufuncs_blas',
-                     sources=['gufuncs_blas.c.src', rearrange],
+                     sources=[in_src('gufuncs_blas.c.src'), rearrange],
                      include_dirs=inc_dirs,
                      extra_info=all_info)
 # =============================================================================
 config.add_extension('_gufuncs_lapack',
-                     sources=['gufuncs_lapack.c.src', rearrange],
+                     sources=[in_src('gufuncs_lapack.c.src'), rearrange],
                      include_dirs=inc_dirs,
                      extra_info=all_info)
 # =============================================================================
 config.add_extension('_gufuncs_lu_solve',
-                     sources=['gufuncs_lu_solve.c.src', rearrange],
+                     sources=[in_src('gufuncs_lu_solve.c.src'), rearrange],
                      include_dirs=inc_dirs,
                      extra_info=all_info)
 # =============================================================================
-config.add_extension('_gufuncs_qr_lstsq',
+config.add_extension(in_src('_gufuncs_qr_lstsq'),
                      sources=['gufuncs_qr_lstsq.c.src', rearrange],
                      include_dirs=inc_dirs,
                      extra_info=all_info)
